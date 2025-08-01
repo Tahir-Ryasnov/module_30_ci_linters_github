826ed187234e2079277eb7757c50707a2ff97d4f
Tahir <a_ryasnov_a@mail.ru>
2025-08-01 09:06:48 +0300

    commit 9:06

--- hw/app/models.py
+++ hw/app/models.py
@@ -54,11 +54,7 @@ class ClientParking(db.Model):
     time_out = db.Column(db.DateTime)
 
     __table_args__ = (
-        db.UniqueConstraint(
-            "client_id",
-            "parking_id",
-            name="unique_client_parking"
-        ),
+        db.UniqueConstraint("client_id", "parking_id", name="unique_client_parking"),
     )
 
     client = db.relationship("Client", back_populates="parkings")
