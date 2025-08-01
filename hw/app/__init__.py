7a783e7bf2d6403ce375082159ae45b91d00d769
Tahir <a_ryasnov_a@mail.ru>
2025-08-01 10:37:34 +0300

    commit 10:37

--- hw/app/__init__.py
+++ hw/app/__init__.py
@@ -20,14 +20,88 @@ def create_app():
 
         db.create_all()
 
+    @app.route(
+        "/clients",
+        methods=[
+            "GET",
+        ],
+    )
+    def get_clients():
+        clients = models.Client.query.all()
+        return (
+            jsonify(
+                [
+                    {
+                        "id": i_client.id,
+                        "name": i_client.name,
+                        "surname": i_client.surname,
+                        "credit_card": i_client.credit_card,
+                        "car_number": i_client.car_number,
+                    }
+                    for i_client in clients
+                ]
+            ),
+            200,
+        )
+
+    @app.route(
+        "/clients/<int:client_id>",
+        methods=[
+            "GET",
+        ],
+    )
+    def get_client(client_id):
+        client = models.Client.query.get_or_404(client_id)
+        return (
+            jsonify(
+                {
+                    "id": client.id,
+                    "name": client.name,
+                    "surname": client.surname,
+                    "credit_card": client.credit_card,
+                    "car_number": client.car_number,
+                }
+            ),
+            200,
+        )
+
+    @app.route(
+        "/clients",
+        methods=[
+            "POST",
+        ],
+    )
+    def create_client():
+        data = request.get_json()
+        client = models.Client(
+            name=data["name"],
+            surname=data["surname"],
+            credit_card=data.get("credit_card"),
+            car_number=data.get("car_number"),
+        )
+        db.session.add(client)
+        db.session.commit()
+        return jsonify({"message": "client created", "id": client.id}), 201
+
+    @app.route("/parkings", methods=["POST"])
+    def create_parking():
+        data = request.get_json()
+        parking = models.Parking(
+            address=data["address"],
+            opened=data.get("opened", True),
+            count_places=data["count_places"],
+            count_available_places=data["count_places"],
+        )
+        db.session.add(parking)
+        db.session.commit()
+        return jsonify({"message": "Parking created", "id": parking.id}), 201
+
     @app.route("/client_parkings", methods=["POST"])
     def parking_entry():
         data = request.get_json()
         client_id = data["client_id"]
         parking_id = data["parking_id"]
 
-        # Удалена строка: client = models.Client.query.get_or_404(client_id)
-        _ = models.Client.query.get_or_404(client_id)
         parking = models.Parking.query.get_or_404(parking_id)
 
         if not parking.opened:
